706d04b6d8d4112e25ebf41a99b09aac3daf9f97
Jimmy Bustillo <jimmy.bustillo@outlook.com>
2022-03-27 16:23:38 -0600

    agrega función para mostrar error cuando el tweet esta vacío

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 // Variables
 const formulario = document.querySelector("#formulario");
 const listaTweets = document.querySelector("#lista-tweets");
+
 let tweets = [];
 
 // Event Listeners
@@ -13,5 +14,31 @@ function eventListeners() {
 function agregarTweet(e) {
   e.preventDefault();
 
-  console.log("Agregando tweet");
+  // Text area donde el usuario escribe
+  const tweet = document.querySelector("#tweet").value;
+
+  // Validación...
+  if (tweet === "") {
+    mostrarError("Un mensaje no puede ir vacio");
+    return; // evita que se ejecuten más lineas de código
+  } else {
+    console.log(tweet);
+  }
+}
+
+//Mostrar mensaje de error
+function mostrarError(error) {
+  const mensajeError = document.createElement("p");
+  mensajeError.textContent = error;
+  mensajeError.classList.add("error");
+
+  // Insertar en el contenido
+  const contenido = document.querySelector("#contenido");
+  contenido.appendChild(mensajeError);
+
+  // Elimina el error del contenido después de 3 segundos
+  setTimeout(() => {
+    mensajeError.remove();
+  }),
+    3000;
 }
